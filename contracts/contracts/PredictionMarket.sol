5e14ebfc696d3f9646750602150a4165e6f88ca9
dxcmint868 <nddminh2021@gmail.com>
2026-02-11 18:48:09 +0700

    feat: add PRB math library and implement pricing functions in PredictionMarket contract

--- contracts/contracts/PredictionMarket.sol
+++ contracts/contracts/PredictionMarket.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.24;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {UD60x18, ud} from "@prb/math/src/UD60x18.sol";
 
 import {IMatchSettlement} from "./interfaces/IMatchSettlement.sol";
 
@@ -21,9 +22,11 @@ contract PredictionMarket is ReentrancyGuard {
     }
 
     error AlreadyInitialized();
+    error InvalidBScore();
     error InvalidOutcome();
+    error InsufficientLiquidity();
     error NotFinalized();
-    error NotImplemented();
+    error Slippage();
 
     bool public initialized;
     bytes32 public matchId;
@@ -36,6 +39,8 @@ contract PredictionMarket is ReentrancyGuard {
 
     mapping(address => mapping(uint8 => uint256)) public shares;
     mapping(uint8 => uint256) public totalShares;
+
+    uint256 private constant WAD_TO_USDC = 1e12;
 
     event Initialized(
         bytes32 matchId,
@@ -63,6 +68,7 @@ contract PredictionMarket is ReentrancyGuard {
     function initialize(MarketConfig calldata config) external {
         if (initialized) revert AlreadyInitialized();
         if (config.outcomesCount < 2) revert InvalidOutcome();
+        if (config.bScore == 0) revert InvalidBScore();
 
         initialized = true;
         matchId = config.matchId;
@@ -88,6 +94,49 @@ contract PredictionMarket is ReentrancyGuard {
         return matchSettlement.getOutcome(matchId);
     }
 
+    function price(uint8 outcome) external view returns (uint256 priceWad) {
+        if (outcome >= outcomesCount) revert InvalidOutcome();
+        UD60x18 b = ud(bScore);
+        UD60x18 sumExp = ud(0);
+        UD60x18 outcomeExp = ud(0);
+
+        for (uint8 i = 0; i < outcomesCount; i++) {
+            UD60x18 expTerm = ud(totalShares[i]).div(b).exp();
+            sumExp = sumExp.add(expTerm);
+            if (i == outcome) {
+                outcomeExp = expTerm;
+            }
+        }
+
+        if (sumExp.unwrap() == 0) return 0;
+        priceWad = outcomeExp.div(sumExp).unwrap();
+    }
+
+    function quoteBuy(
+        uint8 outcome,
+        uint256 amountShares
+    ) public view returns (uint256 costUsdc, uint256 feeUsdc) {
+        if (outcome >= outcomesCount) revert InvalidOutcome();
+        if (amountShares == 0) return (0, 0);
+
+        uint256 costWad = _tradeCostWad(outcome, amountShares, true);
+        costUsdc = _toUsdcUp(costWad);
+        feeUsdc = (costUsdc * feeBps) / 10_000;
+    }
+
+    function quoteSell(
+        uint8 outcome,
+        uint256 amountShares
+    ) public view returns (uint256 proceedsUsdc, uint256 feeUsdc) {
+        if (outcome >= outcomesCount) revert InvalidOutcome();
+        if (amountShares == 0) return (0, 0);
+        if (amountShares > totalShares[outcome]) revert InsufficientLiquidity();
+
+        uint256 proceedsWad = _tradeCostWad(outcome, amountShares, false);
+        proceedsUsdc = _toUsdcDown(proceedsWad);
+        feeUsdc = (proceedsUsdc * feeBps) / 10_000;
+    }
+
     function buy(
         uint8 outcome,
         uint256 amountShares,
@@ -96,19 +145,18 @@ contract PredictionMarket is ReentrancyGuard {
         if (outcome >= outcomesCount) revert InvalidOutcome();
         if (amountShares == 0) return;
 
-        // TODO: LMSR pricing implementation. For now revert to avoid incorrect accounting.
-        // uint256 cost = quoteBuy(outcome, amountShares);
-        // uint256 fee = (cost * feeBps) / 10_000;
-        // require(cost + fee <= maxCost, "slippage");
-        // usdc.safeTransferFrom(msg.sender, address(this), cost + fee);
-        // if (fee > 0) usdc.safeTransfer(feeCollector, fee);
-        // shares[msg.sender][outcome] += amountShares;
-        // totalShares[outcome] += amountShares;
-        // emit Trade(msg.sender, outcome, int256(amountShares), cost, fee);
-        //
-        // NOTE: price curve and q_i updates are pending.
+        (uint256 costUsdc, uint256 feeUsdc) = quoteBuy(outcome, amountShares);
+        if (costUsdc + feeUsdc > maxCost) revert Slippage();
 
-        revert NotImplemented();
+        usdc.safeTransferFrom(msg.sender, address(this), costUsdc + feeUsdc);
+        if (feeUsdc > 0) {
+            usdc.safeTransfer(feeCollector, feeUsdc);
+        }
+
+        shares[msg.sender][outcome] += amountShares;
+        totalShares[outcome] += amountShares;
+
+        emit Trade(msg.sender, outcome, amountShares, costUsdc, feeUsdc);
     }
 
     function sell(
@@ -119,8 +167,28 @@ contract PredictionMarket is ReentrancyGuard {
         if (outcome >= outcomesCount) revert InvalidOutcome();
         if (amountShares == 0) return;
 
-        // TODO: LMSR pricing implementation.
-        revert NotImplemented();
+        uint256 userShares = shares[msg.sender][outcome];
+        if (userShares < amountShares) revert Slippage();
+        if (amountShares > totalShares[outcome]) revert InsufficientLiquidity();
+
+        (uint256 proceedsUsdc, uint256 feeUsdc) = quoteSell(
+            outcome,
+            amountShares
+        );
+        uint256 netProceeds = proceedsUsdc - feeUsdc;
+        if (netProceeds < minProceeds) revert Slippage();
+
+        shares[msg.sender][outcome] = userShares - amountShares;
+        totalShares[outcome] -= amountShares;
+
+        if (feeUsdc > 0) {
+            usdc.safeTransfer(feeCollector, feeUsdc);
+        }
+        if (netProceeds > 0) {
+            usdc.safeTransfer(msg.sender, netProceeds);
+        }
+
+        emit Trade(msg.sender, outcome, amountShares, proceedsUsdc, feeUsdc);
     }
 
     function redeem(uint8 outcome) external nonReentrant {
@@ -133,11 +201,48 @@ contract PredictionMarket is ReentrancyGuard {
 
         shares[msg.sender][outcome] = 0;
 
-        uint256 payout = outcome == winningOutcome ? userShares : 0;
+        uint256 payout = outcome == winningOutcome
+            ? _toUsdcDown(userShares)
+            : 0;
         if (payout > 0) {
             usdc.safeTransfer(msg.sender, payout);
         }
 
         emit Redeemed(msg.sender, outcome, userShares, payout);
+    }
+
+    function _tradeCostWad(
+        uint8 outcome,
+        uint256 amountShares,
+        bool isBuy
+    ) internal view returns (uint256 costWad) {
+        UD60x18 b = ud(bScore);
+        UD60x18 sumBefore = ud(0);
+        UD60x18 sumAfter = ud(0);
+
+        for (uint8 i = 0; i < outcomesCount; i++) {
+            uint256 qi = totalShares[i];
+            uint256 qiAfter = qi;
+            if (i == outcome) {
+                qiAfter = isBuy ? qi + amountShares : qi - amountShares;
+            }
+
+            sumBefore = sumBefore.add(ud(qi).div(b).exp());
+            sumAfter = sumAfter.add(ud(qiAfter).div(b).exp());
+        }
+
+        uint256 costBefore = b.mul(sumBefore.ln()).unwrap();
+        uint256 costAfter = b.mul(sumAfter.ln()).unwrap();
+
+        costWad = isBuy ? costAfter - costBefore : costBefore - costAfter;
+    }
+
+    function _toUsdcDown(uint256 wadAmount) internal pure returns (uint256) {
+        return wadAmount / WAD_TO_USDC;
+    }
+
+    function _toUsdcUp(uint256 wadAmount) internal pure returns (uint256) {
+        if (wadAmount == 0) return 0;
+        return (wadAmount + WAD_TO_USDC - 1) / WAD_TO_USDC;
     }
 }
